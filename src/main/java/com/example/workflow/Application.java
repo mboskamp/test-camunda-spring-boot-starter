3af7eaa93f78f33240197e55bd73521ba02dd399
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2021-10-25 16:53:20 +0200

    try fumbling with classloaders

--- src/main/java/com/example/workflow/Application.java
+++ src/main/java/com/example/workflow/Application.java
@@ -1,5 +1,10 @@
 package com.example.workflow;
 
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.ibatis.io.Resources;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +12,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
   public static void main(String... args) {
+    Map<String, String> classloaderReplacements = new HashMap<>();
+    classloaderReplacements.put(
+        "org/camunda/bpm/engine/impl/mapping/entity/HistoricProcessInstance.xml",
+        "org/camunda/bpm/engine/impl/mapping/entity/replaced-HistoricProcessInstance.xml");
+    classloaderReplacements.put(
+        "org/camunda/bpm/cockpit/impl/plugin/history/historicProcessInstance.xml",
+        "org/camunda/bpm/cockpit/impl/plugin/history/replaced-historicProcessInstance.xml");
+
+    Resources.setDefaultClassLoader(new ClassLoader() {
+      public InputStream getResourceAsStream(String name) {
+        String replacingResource = classloaderReplacements.get(name);
+        if (replacingResource != null) {
+          return Application.class.getClassLoader().getResourceAsStream(replacingResource);
+        }
+        else {
+          return null;
+        }
+      }
+    });
+
     SpringApplication.run(Application.class, args);
   }
 
